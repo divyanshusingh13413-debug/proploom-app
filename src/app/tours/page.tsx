a098b419ebfe72cd9e696efc257215e8916b0ac1
divyanshu singh <divyanshusingh13413@gmail.com>
2026-01-05 01:17:47 +0000

    yaha maina yaak box banaya hia iss ma virtual tour likha do bold font ma

--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -2,6 +2,10 @@
 
 export default function VirtualTourPage() {
   return (
-    <div />
+    <div>
+      <h1 className="text-2xl font-bold tracking-tight font-headline">
+        Virtual Tour
+      </h1>
+    </div>
   );
 }
