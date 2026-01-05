a2198d899801cabbb6e85084f0fd21d5e2a1493c
divyanshu singh <divyanshusingh13413@gmail.com>
2026-01-05 03:13:10 +0000

    yaa command run karo terminal ma (npm run build)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
 'use client';
-import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import AppLayout from '@/components/layout/app-layout';
 import { FirebaseProvider } from '@/firebase/provider';
-
-const metadata: Metadata = {
-  title: 'PropCall 360',
-  description: 'Advanced Real Estate Agent Toolkit',
-};
 
 export default function RootLayout({
   children,
